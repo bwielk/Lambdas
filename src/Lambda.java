882dc0b4afd43ed5814d0f499e5e7874ab12df43
bwielk <bwielk@gmail.com>
2018-05-09 20:26:24 +0200

    Including interface instances in lambda expression

--- src/Lambda.java
+++ src/Lambda.java
@@ -28,12 +28,28 @@ public class Lambda {
 //                return Integer.valueOf(employee2.getAge()).compareTo(Integer.valueOf(employee1.getAge()));
 //            }
 //        });
-        Collections.sort(employees, (Employee e1, Employee e2) ->
+        Collections.sort(employees, (e1, e2) ->
             e1.getName().compareTo(e2.getName()));
 
         for(Employee e : employees){
             System.out.println(e.getName() + " age => " + e.getAge());
         }
+
+//        String aString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//        employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(aString);
+
+        UpperConcat uc = ((s1, s2) -> s1.toUpperCase() + s2.toUpperCase());
+        System.out.println(doStringStuff(uc, employees.get(1).getName(), employees.get(2).getName()));
+    }
+
+    public static final String doStringStuff(UpperConcat u1, String s1, String s2){
+        return u1.upperAndConcat(s1, s2);
     }
 }
 
@@ -61,4 +77,8 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
