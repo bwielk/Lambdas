3dce16f59411197d3f51061462041709b7c61e72
bwielk <bwielk@gmail.com>
2018-05-12 23:39:24 +0200

    Added a Function interface instances

--- src/Employees.java
+++ src/Employees.java
@@ -1,18 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Employees {
 
     public static void main(String[] args) {
 
-        Employee employee1 = new Employee("Blazej", 21);
-        Employee employee2 = new Employee("Tomasso", 32);
-        Employee employee3 = new Employee("Simon", 43);
-        Employee employee4 = new Employee("Bartolomeu", 33);
-        Employee employee5 = new Employee("Christopher", 26);
-        Employee employee6 = new Employee("Christian", 39);
+        Employee employee1 = new Employee("Blazej Wielk", 21);
+        Employee employee2 = new Employee("Tomasso Angelo", 32);
+        Employee employee3 = new Employee("Simon Smith", 43);
+        Employee employee4 = new Employee("Bartolomeu Diaz", 33);
+        Employee employee5 = new Employee("Christopher Baileys", 26);
+        Employee employee6 = new Employee("Christian Odiour", 39);
 
         List<Employee> employees = new ArrayList<>();
         employees.add(employee1);
@@ -31,6 +34,8 @@ public class Employees {
             }
         });
 
+        //PREDICATE
+
         IntPredicate greaterThan10 = i -> i>10;
         IntPredicate lessThan12 = i -> i<12;
         IntPredicate lessThan100 = i -> i <100;
@@ -40,6 +45,33 @@ public class Employees {
         System.out.println("\n4 " + greaterThan10.and(lessThan100).test(20));
         System.out.println("\n5 " + greaterThan10.and(lessThan100).test(1));
         System.out.println("\n6 " + greaterThan10.and(lessThan100).test(111));
+        System.out.println("\n7 " + greaterThan10.equals(111));
+
+        //SUPPLIER
+
+        Random random = new Random();
+        Supplier<Integer> randomSupplier = () -> random.nextInt(100);
+        for(int i=0; i<10; i++){
+            System.out.println("--------------" + (i+1) + "---------------");
+            System.out.println(random.nextBoolean());
+            System.out.println(randomSupplier.get());
+            System.out.println(random.nextDouble());
+            System.out.println(random.nextGaussian());
+        }
+
+        //FUNCTION INTERFACE
+
+        Function<Employee, String> getLastName = (Employee e) -> {
+            return e.getName().substring(e.getName().indexOf(' ') + 1);
+        };
+
+        System.out.println(getLastName.apply(employees.get(1)));
+
+        Function<Employee, String> getFirstName = (Employee e) -> {
+            return e.getName().substring(0, e.getName().indexOf(' '));
+        };
+
+        System.out.println(getFirstName.apply(employees.get(1)));
     }
 
     private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> condition){
