9e38307e10dbf4f776cb6e357c85ca93841997c1
bwielk <bwielk@gmail.com>
2018-05-12 14:50:10 +0200

    More own lambda examples

--- src/LambdaMyExamples.java
+++ src/LambdaMyExamples.java
@@ -26,6 +26,31 @@ public class LambdaMyExamples {
 
         cac.countAndCompare(s1, s2);
         cac.countAndCompare("Abcefg","Hijklmn");
+
+        Add a = (num1, num2) -> {
+            return num1 + num2;
+        };
+
+        System.out.println("The result of adding => " + a.addNumbers(12, 3));
+
+        Multiply m = (num1, num2) -> {
+            return num1*num2*num1*num2;
+        };
+
+        System.out.println("The result of multiplying => " + m.multiplyNums(1,2));
+
+        Multiply m1 = (num1, num2) -> {
+            Double d = (Math.pow(num1, 3) + Math.pow(num2, 3));
+            return d.intValue();
+        };
+
+        Multiply m2 = (num1, num2) -> {
+            return num1*num2*20;
+        };
+
+        System.out.println("The result of multiplying m2 => " +  m2.multiplyNums(6,7));
+
+        System.out.println("The result of multiplying m1 => " + m1.multiplyNums(2, 5));
     }
 }
 
@@ -35,4 +60,12 @@ interface CountsCharacters {
 
 interface CountsCharsAndComparesStrings {
     public void countAndCompare(String s1, String s2);
+}
+
+interface Add {
+    public int addNumbers(int a, int b);
+}
+
+interface Multiply {
+    public int multiplyNums(int a, int c);
 }
