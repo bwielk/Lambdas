4fabb42fdba6779cb9edeecb243211557be0dee6
bwielk <bwielk@gmail.com>
2018-05-12 23:17:28 +0200

    More predicates

--- src/Employees.java
+++ src/Employees.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
 public class Employees {
@@ -23,6 +24,22 @@ public class Employees {
 
         printEmployeesByAge(employees, "Employees over 30", employee -> employee.getAge()>=30);
         printEmployeesByAge(employees, "Employees under 30", employee -> employee.getAge()<30);
+        printEmployeesByAge(employees, "Employees under 25", new Predicate<Employee>() {
+            @Override
+            public boolean test(Employee employee) {
+                return employee.getAge() < 25;
+            }
+        });
+
+        IntPredicate greaterThan10 = i -> i>10;
+        IntPredicate lessThan12 = i -> i<12;
+        IntPredicate lessThan100 = i -> i <100;
+        System.out.println("\n1 " + greaterThan10.test(11));
+        System.out.println("\n2 " + greaterThan10.negate().test(11));
+        System.out.println("\n3 " + lessThan12.or(lessThan100).test(99));
+        System.out.println("\n4 " + greaterThan10.and(lessThan100).test(20));
+        System.out.println("\n5 " + greaterThan10.and(lessThan100).test(1));
+        System.out.println("\n6 " + greaterThan10.and(lessThan100).test(111));
     }
 
     private static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> condition){
